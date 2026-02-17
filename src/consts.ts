df7ff29cd5ffb7ff1f0f2f5340bf1ede5527eaa5
hiroebe <hiroebe41@gmail.com>
2026-02-17 21:19:17 +0900

    Update blog title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'hiroebe.net';
-export const SITE_DESCRIPTION = 'hiroebe のブログ';
+export const SITE_TITLE = 'hiroebe のブログ';
+export const SITE_DESCRIPTION = 'hiroebe のブログです。技術的な話もそうでないものも、好きに書いています。';
 
 export const TAGS = {
 	tech: 'tech',
